1e479ba987a5ffccb71787e08598a9e76bd238e8
yangqingluo <84807330@qq.com>
2018-01-18 22:07:38 +0800

    主页导航栏搜索框初步添加

--- HanNiu/MainVCs/Home/HomeViewController.m
+++ HanNiu/MainVCs/Home/HomeViewController.m
@@ -19,19 +19,29 @@
     [super viewDidLoad];
     
 //    self.title = @"";
-    UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 22, 24)];
+    UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 60, 40)];
     [btn setImage:[UIImage imageNamed:@"main_me_icon"] forState:UIControlStateNormal];
+    btn.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
     [btn addTarget:self action:@selector(accountButtonAction) forControlEvents:UIControlEventTouchUpInside];
 //    [self.navigationController.navigationBar addSubview:btn];
     
-    UIBarButtonItem *leftItem = [[UIBarButtonItem alloc] initWithCustomView:btn];
-    [self.navigationItem setLeftBarButtonItem:leftItem animated:YES];
+    UIView *leftView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 60, 40)];
+    leftView.clipsToBounds = NO;
+    [leftView addSubview:btn];
     
+    UIBarButtonItem *leftItem = [[UIBarButtonItem alloc] initWithCustomView:leftView];
+    self.navigationItem.leftBarButtonItems = @[leftItem];
+    
+    UITextField *searchField = [[UITextField alloc] initWithFrame:CGRectMake(-20, 5, screen_width - 2 * kEdgeToScreen, 30)];
+    searchField.borderStyle = UITextBorderStyleRoundedRect;
+    
+    UIView *titleView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, screen_width, 40)];
+    [titleView addSubview:searchField];
+    [self.navigationItem setTitleView:titleView];
 }
 
 - (void)accountButtonAction {
     AccountViewController *vc = [AccountViewController new];
-//    vc.hidesBottomBarWhenPushed = YES;
     [self doPushViewController:vc animated:YES];
 }
 
