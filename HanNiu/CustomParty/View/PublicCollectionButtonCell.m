09d2709bb66e8dc9560ad16531cd974466e79938
yangqingluo <84807330@qq.com>
2018-02-01 10:07:27 +0800

    collection cell缝隙颜色遮蔽处理

--- HanNiu/CustomParty/View/PublicCollectionButtonCell.m
+++ HanNiu/CustomParty/View/PublicCollectionButtonCell.m
@@ -13,7 +13,12 @@
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-        self.contentView.backgroundColor = [UIColor whiteColor];
+//        self.contentView.backgroundColor = [UIColor whiteColor];
+        //某些情况下，一行cell的宽度和并不完全等于collectionView.width，所以cell之间有空隙，会显示出底部其他视图的背景颜色，所以这样处理
+        UIView *baseView = [[UIView alloc] initWithFrame:self.bounds];
+        baseView.width = ceil(baseView.width);
+        baseView.backgroundColor = [UIColor whiteColor];
+        [self insertSubview:baseView belowSubview:self.contentView];
         
         _button = NewButton(CGRectMake(kEdgeSmall, kEdgeSmall, self.contentView.width - 2 * kEdgeSmall, self.contentView.height - 2 * kEdgeSmall), @"", appTextColor, [AppPublic appFontOfSize:appButtonTitleFontSizeLittle]);
         [_button setBackgroundImage:[UIImage imageNamed:@"icon_classify_sub_recycler_item_back_normal"] forState:UIControlStateNormal];
